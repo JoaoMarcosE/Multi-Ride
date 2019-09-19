af2ea562e4f77ddb89d37c271420c1860632f702
Nathaniel Felipe Leicht Zanotto <nathyzanotto@gmail.com>
2019-09-19 20:13:44 -0300

    [IMP] Ajustes e inclusão do page header

---
+++ multi-ride-front/src/components/PageHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="page-header container">
+      <div class="row">
+          <div class="col-sm-3 logo-img">
+              <img src="assets/logo.png" alt="" class="logo-img">
+          </div>
+          <div class="col-sm-9 logo-text">
+              <span class="logo-text">Multi-Ride</span>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PageHeader",
+  props: {
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
