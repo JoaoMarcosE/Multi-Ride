d0cb546c624c7f05c0bd424a8275c2b1b282c11a
JoaoMarcosE <44987093+JoaoMarcosE@users.noreply.github.com>
2019-10-31 20:11:58 -0300

    Commit inicial do projeto do back-end
    Criadas interfaces para construção dos métodos das APIs do Uber e 99.
    Criada interface para API de conexão.
    Criada classe de conexão com a API do Uber, implementada conexão inicial de acordo com instruções da seção de desenvolvedor do site da Uber.

---
+++ multi-ride-back/MultiRide/src/api/ApiUber.java
@@ -0,0 +1,74 @@
+package api;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+public class ApiUber implements IApi {
+
+
+	public static void main(String[] args) {
+		
+		
+		URL obj = null;
+		try {
+			obj = new URL("https://api.uber.com/v1.2/products?latitude=37.7759792&longitude=-122.41823");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    HttpURLConnection con = null;
+		try {
+			con = (HttpURLConnection) obj.openConnection();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	    try {
+			con.setRequestMethod("GET");
+		} catch (ProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    con.setRequestProperty("User-Agent", "Mozilla/5.0");
+		
+	    int responseCode = 0;
+		try {
+			responseCode = con.getResponseCode();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    System.out.println("Response Code : " + responseCode);
+	    BufferedReader in = null;
+		try {
+			in = new BufferedReader(
+			        new InputStreamReader(con.getInputStream()));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    String inputLine;
+	    StringBuffer response = new StringBuffer();
+	    try {
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    
+	    try {
+			in.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
